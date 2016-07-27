33cee4247b086622bbd843742b38f2a6151775e5
Belac Darkstorm <calebq23@gmail.com>
2016-07-27 16:20:48 -0500

    Adding more coverage. Has trouble with the ultimateString

--- bbConv_test.go
+++ bbConv_test.go
@@ -25,3 +25,9 @@ func TestMarkdown(t *testing.T) {
 	})
 	t.Log(conv.Convert(testStr))
 }
+
+func TestAllTags(t *testing.T) {
+	ultimateStr := "[b]Bold[/b][bold]Bold[/bold][i]Italics[/i][italics]Italics[/italics][u]Underline[/u][underline]Underline[/underline][s]Strike[/s][strike]Strike[/strike][font=Verdana]Some Text[/font][font size=20pt]Some Text[/font][font color=red]Some Text[/font][font color=#000000]Some Text[/font][font variant=upper]Some Text[/font][font variant=lower]Some Text[/font][font variant=smallcaps]Some Text[/font][size=20pt]Some Text[/size][color=red]Some Text[/color][color=#000000]Some Text[/color][smallcaps]Some Text[/smallcaps][url]Link address[/url][url=address]Some Text[/url][url title='Title']Link address[/url][link]Link address[/link][link=address]Some Text[/link][link title='Title']Link address[/link][youtube]Youtube URL or video ID[/youtube][youtube height=200 width=500]Youtube URL or video ID[/youtube][youtube=500x200]Youtube URL or video ID[/youtube][youtube left]Youtube URL or video ID[/youtube][youtube right]Youtube URL or video ID[/youtube][img]Image URL[/img][img=500x200]Image URL[/img][img height=200 width=500]Image URL[/img][img left]Image URL[/img][img right]Image URL[/img][img alt='Alternate text']Image URL[/img][img title='Title']Image URL[/img][image]Image URL[/image][title]Some Text[/title][t1]Some Text[/t1][t2]Some Text[/t2][t3]Some Text[/t3][t4]Some Text[/t4][t5]Some Text[/t5][t6]Some Text[/t6][align=center]Some Text[/align][ul]\n* Item 1\nItem 2\n[/ul][ol]\n* Item 1\nItem 2\n[/ol][bullet] * Item 1 * Item 2[/bullet][number] * Item 1 * Item 2[/number][ul]* Item 1 * Item 2[/ul][ol]* Item 1 * Item 2[/ol]"
+	conv := CreateConverter(true, true)
+	t.Log(conv.Convert(ultimateStr))
+}

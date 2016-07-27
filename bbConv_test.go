33ef93066cb85b262c20b7384fce88afd77b16b2
Belac Darkstorm <calebq23@gmail.com>
2016-07-27 13:24:31 -0500

    Added more to test

--- bbConv_test.go
+++ bbConv_test.go
@@ -2,13 +2,23 @@ package bbConvert
 
 import "testing"
 
-var (
-	testString = "[b]This is bold[/b]\n Testing paragraph seperating :)\n [CustomTag]This is going to be ignored[/CustomTag] \n [font='Times New Roman' size=20]fonted[/font]"
-)
-
 func TestWraping(t *testing.T) {
 	AddClass("Classy")
 	ImplementDefaults()
 	SetWrap(true)
-	t.Log(BBtohtml(testString))
+	AddCustomTag("customtag", func(fnt Tag, meat string) string {
+		return "This is custom!"
+	})
+	t.Log(BBtohtml("[b]This is bold[/b]\n Testing paragraph seperating :)\n [CustomTag]This is going to be ignored[/CustomTag] \n [font='Times New Roman' size=20]fonted[/font]"))
+}
+
+func TestAlternate(t *testing.T) {
+	SetWrap(false)
+	AddCustomTag("b", func(fnt Tag, meat string) string {
+		return "**" + meat + "**"
+	})
+	AddCustomTag("i", func(fnt Tag, meat string) string {
+		return "*" + meat + "*"
+	})
+	t.Log(BBtohtml("[b]This should be in markdown style bold[/b] [i]And now markdown italics[/i]"))
 }

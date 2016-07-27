63b3feb733b87421c4a4c699da9f3926010ccc73
Belac Darkstorm <calebq23@gmail.com>
2016-07-27 12:55:18 -0500

    Added bbConv_test.go

---
+++ bbConv_test.go
@@ -0,0 +1,17 @@
+package bbConvert
+
+import "testing"
+
+var (
+	testString = "[b]This is bold[/b]\n Testing paragraph seperating :)\n [CustomTag]This is going to be ignored[/CustomTag]"
+)
+
+func TestConversion(t *testing.T) {
+	AddClass("Classy")
+	ImplementDefaults()
+	AddCustomTag("customtag", func(fnt Tag, meat string) string {
+		return "I'm not caring about the meat right now"
+	})
+	SetWrap(true)
+	t.Log(BBtohtml(testString))
+}
